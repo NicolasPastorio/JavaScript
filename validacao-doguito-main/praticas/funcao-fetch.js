ca3430bf339ba127a3365c409a446f9729803715
Nícolas Pastório de Moura <108685207+NicolasPastorio@users.noreply.github.com>
2023-01-25 17:49:01 -0300

    Add files via upload

---
+++ validacao-doguito-main/praticas/funcao-fetch.js
@@ -0,0 +1,7 @@
+const options = {//requisição
+  method: 'GET',//como queremos
+  mode: 'cors',//usada para fazer chamadas entre aplicações diferentes(opcional)
+  headers: {//oquê queremos
+    'content-type': 'application/json;charset=utf-8'
+  }
+}
