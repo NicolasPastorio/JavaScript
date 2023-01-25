29ea799e2ae8bea8fb044d9af82c35bfa170e07c
Nícolas Pastório de Moura <108685207+NicolasPastorio@users.noreply.github.com>
2023-01-25 17:56:42 -0300

    Delete validacao-doguito-main directory

--- validacao-doguito-main/praticas/funcao-fetch.js
+++
@@ -1,7 +0,0 @@
-const options = {//requisição
-  method: 'GET',//como queremos
-  mode: 'cors',//usada para fazer chamadas entre aplicações diferentes(opcional)
-  headers: {//oquê queremos
-    'content-type': 'application/json;charset=utf-8'
-  }
-}
